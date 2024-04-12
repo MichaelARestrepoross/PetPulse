ff77bd9364ebf09265c794a0f926b5b55d5436f5
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-04-12 13:39:22 -0400

    Created and updated delete to work for pets. as well as naviation to edit

--- src/Components/PetDetailed.jsx
+++ src/Components/PetDetailed.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'; 
+import { useParams, useNavigate } from 'react-router-dom'; 
 import ReminderSingle from './ReminderSingle';
 
 function PetDetailed() {
   const [pet, setPet] = useState(null);
   const { id } = useParams();
-
+  const navigate = useNavigate();
+  
   useEffect(() => {
     const fetchPetDetails = async () => {
       const token = localStorage.getItem("token");
@@ -24,7 +25,35 @@ function PetDetailed() {
       }
     };
     fetchPetDetails();
-  }, []);
+  }, [id]);
+
+  const handleDelete = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      if (token) {
+        const response = await fetch(`http://localhost:3003/api/pets/${id}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (response.ok) {
+          // Pet deleted successfully, navigate to pets / petsindex
+          navigate('/pets');
+        } else {
+          // Error if delete request fails
+          console.error("Failed to delete pet");
+        }
+      }
+    } catch (error) {
+      console.error("Error deleting pet:", error);
+    }
+  };
+
+  const handleEdit = () => {
+    navigate(`/pets/${id}/edit`);
+  };
+
 
   return (
     <div className="max-w-xl mx-auto">
@@ -38,6 +67,8 @@ function PetDetailed() {
             <p className="text-gray-700"><strong>Age:</strong> {pet.age || "Unknown"}</p>
             <p className="text-gray-700"><strong>Created At:</strong> {new Date(pet.created_at).toLocaleString()}</p>
             <p className="text-gray-700"><strong>Updated At:</strong> {new Date(pet.updated_at).toLocaleString()}</p>
+            <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mr-2" onClick={handleDelete}>Delete</button>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" onClick={handleEdit}>Edit</button>
           </div>
           <div className="px-4 py-2 ">
             <h2 className="text-lg font-semibold text-gray-800 mb-2">Filtered Reminders</h2>
