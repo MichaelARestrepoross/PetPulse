8a2c38b2eb77afd6c5b3e1059616b6fa7cb5a1de
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-04-15 15:37:53 -0400

    Updated petDeatiled image

--- src/Components/PetDetailed.jsx
+++ src/Components/PetDetailed.jsx
@@ -64,14 +64,14 @@ function PetDetailed() {
         <div className="bg-white shadow-md rounded-lg overflow-hidden">
           <div className="px-4 py-2">
               {/* Image div */}
-              <div className="relative overflow-hidden rounded-lg" style={{ paddingTop: '100%' }}>
-                {console.log(pet.image_url)}
-                <img
-                  className="absolute inset-0 w-full h-full object-cover"
-                  src={pet.image_url}
-                  alt={pet.name}
-                />
-              </div>
+          <div className="relative overflow-hidden rounded-lg" style={{ width: '100%', height: '400px' }}>
+            {console.log(pet.image_url)}
+            <img
+              className="absolute inset-0 w-full h-full object-cover"
+              src={pet.image_url}
+              alt={pet.name}
+            />
+          </div>
             <h2 className="text-lg font-semibold text-gray-800 mb-2">Pet Details</h2>
             <p className="text-gray-700"><strong>Name:</strong> {pet.name}</p>
             <p className="text-gray-700"><strong>Species:</strong> {pet.species}</p>
@@ -80,7 +80,7 @@ function PetDetailed() {
             <p className="text-gray-700"><strong>Created At:</strong> {new Date(pet.created_at).toLocaleString()}</p>
             <p className="text-gray-700"><strong>Updated At:</strong> {new Date(pet.updated_at).toLocaleString()}</p>
             <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mr-2" onClick={handleDelete}>Delete</button>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" onClick={handleEdit}>Edit</button>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-2" onClick={handleEdit}>Edit</button>
             <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-2" onClick={handleCreateReminder}>Create Reminder</button>
           </div>
           <div className="px-4 py-2 ">
