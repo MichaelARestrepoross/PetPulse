8f64fbcd8d7b9bc58ae8efc958c166f70ea6761b
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-04-16 10:53:13 -0400

    Updated pets form

--- src/Components/ReminderForm.jsx
+++ src/Components/ReminderForm.jsx
@@ -23,6 +23,12 @@ const ReminderForm = () => {
   const [createdAt, setCreatedAt] = useState(null);
   const [updatedAt, setUpdatedAt] = useState(null);
 
+    // Function to format reminder time
+    const formatReminderTime = (timeString) => {
+      const options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+      return new Date(timeString).toLocaleString(undefined, options);
+    };
+
   useEffect(() => {
     if (!isNewReminder && reminderId) {
       // Fetch reminder details if editing and reminderId is defined
@@ -35,8 +41,11 @@ const ReminderForm = () => {
           const data = await response.json();
           
           // Extract date and time portion and remove milliseconds
-          const formattedReminderTime = data.reminder_time.slice(0, 16);
-  
+          // const formattedReminderTime = data.reminder_time.slice(0, 16);
+          console.log("unEdited:",data.reminder_time);
+          const formattedReminderTime = formatReminderTime(data.reminder_time);
+          console.log("Edited:",formattedReminderTime);
+
           setFormData({
             ...data,
             reminder_time: formattedReminderTime,
@@ -107,10 +116,17 @@ const ReminderForm = () => {
   
 
   return (
-    <div className="container mx-auto">
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+    <div className="container mx-auto border p-2">
+      <form onSubmit={handleSubmit} 
+        className="max-w-md mx-auto border "
+      >
+        <div className="flex items-center justify-center border h-10 bg-blue-700 bg-gradient-to-tr from-blue-400 to-transparent">
+          <p className="text-white">Pets Form</p>
+        </div>
+      <div className="flex flex-col items-center justify-center p-2">
+
         <div className="mb-4">
-          <label htmlFor="pet_id" className="block text-sm font-medium text-gray-700">
+          <label htmlFor="pet_id" className="block text-sm font-medium text-gray-700 text-center w-full">
             Pet ID
           </label>
           <input
@@ -120,13 +136,13 @@ const ReminderForm = () => {
             value={formData.pet_id}
             onChange={handleInputChange}
             required
-            className="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
-          />
+            className="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 w-60"
+            />
         </div>
         <div className="mb-4">
-          <label htmlFor="reminder_type" className="block text-sm font-medium text-gray-700">
-            Reminder Type
-          </label>
+        <label htmlFor="reminder_type" className="block text-sm font-medium text-gray-700 text-center w-full">
+          Reminder Type
+        </label>
           <input
             type="text"
             id="reminder_type"
@@ -134,24 +150,25 @@ const ReminderForm = () => {
             value={formData.reminder_type}
             onChange={handleInputChange}
             required
-            className="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
-          />
+            className="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 w-60"
+            />
         </div>
         <div className="mb-4">
-          <label htmlFor="reminder_message" className="block text-sm font-medium text-gray-700">
+          <label htmlFor="reminder_message" className="block text-sm font-medium text-gray-700 text-center w-full">
             Reminder Message
           </label>
-          <input
-            type="text"
+          <textarea
             id="reminder_message"
             name="reminder_message"
             value={formData.reminder_message}
             onChange={handleInputChange}
-            className="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+            className="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500 w-60"
+            rows="4" 
           />
         </div>
+
         <div className="mb-4">
-          <label htmlFor="reminder_time" className="block text-sm font-medium text-gray-700">
+          <label htmlFor="reminder_time" className="block text-sm font-medium text-gray-700 text-center w-full">
             Reminder Time
           </label>
           <input
@@ -161,20 +178,20 @@ const ReminderForm = () => {
             value={formData.reminder_time}
             onChange={handleInputChange}
             className="mt-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
-          />
+            />
         </div>
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
+          >
           {isNewReminder ? "Create Reminder" : "Update Reminder"}
         </button>
+      </div>
       </form>
       {/* Display created and updated dates */}
       {!isNewReminder && (
         <div className="mt-4 text-gray-700">
           <p>Created At: {createdAt}</p>
-          <p>Updated At: {updatedAt}</p>
         </div>
       )}
     </div>
