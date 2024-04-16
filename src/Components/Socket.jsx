daae2dc511b630653e91b9347a732e441ce877b6
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-04-16 15:58:05 -0400

    Created and updated modal and updated socket. times still need updating

--- src/Components/Socket.jsx
+++ src/Components/Socket.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from "react";
 
-function Socket() {
-    const [remidners, setReminders]= useState([]);
-    const [isModalOpen,setIsModalOpen] = useState(false);
-    const [modalContent, setModalContent] = useState("");
+const formattedDate = new Intl.DateTimeFormat("en-US", {
+  year: "numeric",
+  month: "long",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  //   second: "2-digit",
+  hour12: true, // Use 12-hour time; set to false for 24-hour time
+});
 
+const SocketComponent = () => {
+  const [reminders, setReminders] = useState([]);
 
-}
+  useEffect(() => {
+    fetch("http://localhost:3003/reminders/1")
+      .then((res) => res.json())
+      .then((data) => setReminders(data));
+  }, []);
 
-export default Socket
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h1>Reminders</h1>
+      <ul>
+        {reminders &&
+          reminders.map((reminder) => (
+            <ol key={reminder.id}>
+              {reminder.title} -{" "}
+              {formattedDate.format(new Date(reminder.reminder_time))}
+            </ol>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SocketComponent;
