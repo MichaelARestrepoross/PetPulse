982f88a79564011a974ce39efd0c33cfe735ab11
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-04-16 12:09:42 -0400

    got rid of uneeded function

--- src/Components/ReminderForm.jsx
+++ src/Components/ReminderForm.jsx
@@ -35,18 +35,6 @@ const ReminderForm = () => {
       return `${year}-${month}-${day}T${hours}:${minutes}`;
     };
 
-    const convertToBackendFormat = (date) => {
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, "0");
-      const day = String(date.getDate()).padStart(2, "0");
-      const hours = String(date.getHours()).padStart(2, "0");
-      const minutes = String(date.getMinutes()).padStart(2, "0");
-      const seconds = String(date.getSeconds()).padStart(2, "0");
-      const milliseconds = String(date.getMilliseconds()).padStart(3, "0");
-    
-      return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`;
-    };
-
   useEffect(() => {
     if (!isNewReminder && reminderId) {
       // Fetch reminder details if editing and reminderId is defined
